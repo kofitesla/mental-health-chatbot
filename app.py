3ec6c7c14cec0922b7b32af73f5112c8717c5bfb
GODFRED ADANE <91817149+kofitesla@users.noreply.github.com>
2025-08-22 07:23:11 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -310,4 +310,5 @@ def internal_error(error):
     return render_template("500.html"), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
